a26c2f07dca2ca5518f8c79e4d519b5bcaf106af
Flor Molina <florm@techfactory.com.pe>
2024-05-07 18:00:31 -0500

    11:00

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   // type: 'postgres',
+    //   // host: 'localhost',
+    //   // port: 5432,
+    //   // username: 'postgres',
+    //   // password: 'root',
+    //   // database: 'dashboard',
+    // }),
+    // UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
